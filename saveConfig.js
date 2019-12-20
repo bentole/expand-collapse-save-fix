4c21db8ab7764e811b7451cb48e816cc24f0ba5f
bentole <43088070+bentole@users.noreply.github.com>
2019-12-20 12:36:47 +0100

    Update saveConfig.js

--- saveConfig.js
+++ saveConfig.js
@@ -2,24 +2,26 @@ var collapsedChildren = {}
 
 function removeCC() {
 	cy.nodes().forEach(function(item, index, array) {
-		let id = item.id();
-    	  	let cC = item.data().collapsedChildren
-        	let parentId = item.id();
-        	collapsedChildren[id] = {}
-    		collapsedChildren[id]['data']  = cC.jsons();
-    		delete cy.$id(id).data().collapsedChildren;
+    	  let cC = item.data().collapsedChildren
+	  if ( cC ) {
+	        let id = item.id();
+	        let parentId = item.id();
+	        collapsedChildren[id] = {}
+	    	collapsedChildren[id]['data']  = cC.jsons();
+	    	delete cy.$id(id).data().collapsedChildren;
+	  };
 	});
 };
 function restoreCC() {
-  	for (let parentId in collapsedChildren) {
-    		let arr = collapsedChildren[parentId].data;
-    		cy.$id(parentId).data().collapsedChildren = cy.add(arr);
-    		arr.forEach(function(item, index, array) {
-      			cy.$id(item.data.id).remove();
-    		});
-  	};
+  for (let parentId in collapsedChildren) {
+    	let arr = collapsedChildren[parentId].data;
+    	cy.$id(parentId).data().collapsedChildren = cy.add(arr);
+    	arr.forEach(function(item, index, array) {
+		cy.$id(item.data.id).remove();
+	});
+  };
 };
 removeCC();
-//console.log(JSON.stringify(cy.json()));
+$.post(".", { 'save_config': JSON.stringify(cy.json()) }, null, 'json');
 //console.log(JSON.stringify(collapsedChildren));
 restoreCC();
